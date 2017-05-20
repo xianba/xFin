b7a15e443b3611fdef90f073ebac9e60c0862819
xianba xia <778757080@qq.com>
2017-05-20 19:40:36 +0800

    Update main.py

--- finlib/main.py
+++ finlib/main.py
@@ -1,9 +1,15 @@
 import sys
+import argparse
 
 def main(args=None):
     """The main routine."""
     if args is None:
         args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("echo",help="echo the string you use here")
+    args = parser.parse_args()
+    print args.echo
 
     print("This is the main routine.")
     print("It should do something interesting.")
