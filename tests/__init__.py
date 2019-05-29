84d3e4cb0141530745fd9348b3a718a7fe2e2a7e
xianba xia <778757080@qq.com>
2019-05-29 10:15:05 +0800

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py.py
+@Version :   1.0.0
+@Author  :   xiaxianba
+@License :   (C) Copyright 2006-2019, Proya
+@Contact :   xiaxianba@qq.com
+@Software:   PyCharm
+@Time    :   2019/5/6 15:05
+@Desc    :
+'''
