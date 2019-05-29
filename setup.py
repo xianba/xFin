47b6727cafbb29f13eb0c2701c387b2e89185d09
xianba xia <778757080@qq.com>
2019-05-29 10:21:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,28 @@
 
 from setuptools import setup
 
+long_desc = """
+xFin
+===============
+
+Target Users
+--------------
+* financial market analyst of China
+* learners of financial data analysis with pandas/NumPy
+* people who are interested in China financial data
+Installation
+--------------
+    pip install xfin
+
+Upgrade
+---------------
+    pip install xfin --upgrade
+"""
+
 setup(name='xfin',
       version='1.0.0',
       packages=['xfin'],
+      long_description=long_desc,
       entry_points={
           'console_scripts': [
               'xfin = xfin.main:main'
