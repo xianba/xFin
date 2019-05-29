8c20393e963f633b081d5be24daa8c4b6b0b45b8
xianba xia <778757080@qq.com>
2019-05-29 10:46:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,12 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-import codecs
-import os
-
-def read(fname):
-    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-long_desc = """
-xFin
-===============
-
-Target Users
---------------
-* financial market analyst of China
-* learners of financial data analysis with pandas/NumPy
-* people who are interested in China financial data
-Installation
---------------
-    pip install xfin
-
-Upgrade
----------------
-    pip install xfin --upgrade
-"""
 
 setup(name='xfin',
       version='1.0.2',
       packages=['xfin'],
-      long_description=long_desc,
+      long_description = open("README.md").read(),
+      long_description_content_type="text/markdown",
       entry_points={
           'console_scripts': [
               'xfin = xfin.main:main'
